6d2e46ddbb54c1448e84faa162c275569c6a21be
srebalaji <srebalaji@outlook.com>
2017-05-20 13:43:14 +0530

    Second scenario
    Added validations to article model. And updated rspec to pass the test cases.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+	validates :title, :description, presence: true
 end
--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -14,4 +14,20 @@ RSpec.feature "Creating Articles" do
 		expect(page).to have_content "Article was successfully created."
 		expect(page.current_path).to eq(articles_path)
 	end
+
+	scenario "User created an article without entering body and Description" do 
+		visit "/"
+
+		click_link "Create Article"
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: ""
+
+		click_button "Create Article"
+
+		expect(page).to have_content "Some errors prohibited this article from being saved:"
+		expect(page).to have_content "Title can't be blank"
+		expect(page).to have_content "Description can't be blank"
+		expect(page.current_path).to eq(articles_path)
+	end
 end

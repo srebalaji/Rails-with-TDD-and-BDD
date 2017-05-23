ddf8bbf2a795f783de2916256040b176458ec339
srebalaji <srebalaji@outlook.com>
2017-05-24 00:51:03 +0530

    Adding no articles listing test case
    Added no artcile listing test case to display text if there is no articles present. And made the test case pass.

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -15,4 +15,18 @@ RSpec.feature "Creating Articles" do
 		expect(page).to have_content @article2.title
 		expect(page).to have_content @article2.description
 	end
+
+	scenario "Displaying no articles in the home page" do
+		Article.delete_all
+		visit '/'
+
+		expect(page).not_to have_content @article1.title
+		expect(page).not_to have_content @article1.description
+		expect(page).not_to have_content @article2.title
+		expect(page).not_to have_content @article2.description
+
+		within("h1#no_article") do
+			expect(page).to have_content "No articles present"
+		end
+	end
 end
